a2cc778786c712bbd56adf4ab4f4619b0d288261
Fabian FACINOU <20191076@b15-12.local>
2019-11-21 16:05:39 +0100

    server http (GET to PUSH)

---
+++ consignes.js
@@ -0,0 +1,8 @@
+// [1] Create server http with command line: node main.js <PORT>
+// [2] GET http://localhost:PORT --> <h1>Hello, World!</h1>
+// [3] GET    /?name=Ch0pper --> <h1>Hello, Ch0pper!</h1>
+// [4] POST   /students/ with {name: "Zoro", school: "Efrei"}
+//  --> create students.json on server side + add [id] to user
+// [5] PUT    /students/1 update students with id 1 on file
+// [6] DELETE /students/1 delete the student
+// [7] DELETE /students delete all students
